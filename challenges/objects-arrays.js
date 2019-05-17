c06ae2f13bd6d03aaf3edaef6cb199ef071cc27a
Shola Ayeni <ayenisholah@yahoo.com>
2019-05-17 08:20:51 +0100

    Dinosaur object created

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,6 +6,13 @@
   object name, diet, weight, length, period
 */
 
+const name = [
+  {"name": "tyrannosaurus", "diet": "carnivorous", "weight": "7000kg", "length": "12m", "period": "Late Cretaceous"},
+  {"name": "stegosaurus", "diet": "herbivorous", "weight": "2000kg", "length": "9m", "period": "Late Jurassic"},
+  {"name": "velociraptor", "diet": "carnivorous", "weight": "1.8m", "length": "12m", "period": "Late Cretaceous"},
+  {"name": "tyrannosaurus", "diet": "carnivorous", "weight": "7000kg", "length": "12m", "period": "Late Cretaceous"}
+]
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
