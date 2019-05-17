bf8116166d9f1c610f9b199814675e2822cac0ad
Shola Ayeni <ayenisholah@yahoo.com>
2019-05-17 08:47:55 +0100

    Array Request 1 & 2

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -39,18 +39,20 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
-
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+graduates.forEach(function(element){
+  let uni_list = element.university;
+  universities.push(uni_list);
+})
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
-
-The resulting contact information should have a space between the first name and the email information like this: 
-Name email@example.com
-
-Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(function(element){
+  let Info = `${element.first_name} ${element.email}`;
+  contactInfo.push(Info);
+})
 console.log(contactInfo);
 
 
